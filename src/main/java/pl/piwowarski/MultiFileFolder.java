48bb844c11dcc0fdd8c31936dbbec4335f53b0f6
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-13 13:37:40 +0200

    verify that folders is not null

--- src/main/java/pl/piwowarski/MultiFileFolder.java
+++ src/main/java/pl/piwowarski/MultiFileFolder.java
@@ -1,13 +1,20 @@
 package pl.piwowarski;
 
 import java.util.List;
+import java.util.Objects;
 
 class MultiFileFolder extends SingleFileFolder implements MultiFolder{
     private final List<Folder> folders;
 
     public MultiFileFolder(String name, String size, List<Folder> folders) {
         super(name, size);
-        this.folders = folders;
+        if (folders == null){
+            this.folders = List.of();
+        }else {
+            this.folders = folders.stream()
+                    .filter(Objects::nonNull)
+                    .toList();
+        }
     }
 
     @Override
