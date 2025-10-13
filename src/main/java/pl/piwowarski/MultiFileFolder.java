6253346ea544380a9dbf3ae889d80add2ccdf325
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-13 14:35:08 +0200

    added null verification for list

--- src/main/java/pl/piwowarski/MultiFileFolder.java
+++ src/main/java/pl/piwowarski/MultiFileFolder.java
@@ -1,16 +1,20 @@
 package pl.piwowarski;
 
 import java.util.List;
+import java.util.Objects;
 
-class MultiFileFolder extends SingleFileFolder implements MultiFolder{
+class MultiFileFolder extends SingleFileFolder implements MultiFolder {
     private final List<Folder> folders;
 
     public MultiFileFolder(String name, String size, List<Folder> folders) {
         super(name, size);
-        if (folders == null){
+        if (folders == null) {
             this.folders = List.of();
-        }else {
-            this.folders = folders;
+        } else {
+            this.folders = folders.stream()
+                    .filter(Objects::nonNull)
+                    .toList()
+            ;
         }
     }
 
